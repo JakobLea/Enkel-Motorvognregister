d8c53ab514aaa0e06b50bff15e7b5ad60737e3a7
JakobLea <jakob.lea@outlook.com>
2023-12-17 16:49:06 +0100

    Initial commit

---
+++ src/main/java/com/example/bilregister/BilregisterApplication.java
@@ -0,0 +1,13 @@
+package com.example.bilregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilregisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilregisterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bilregister/BilregisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bilregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilregisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
