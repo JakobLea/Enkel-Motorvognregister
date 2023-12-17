662d6d075e1e1619a4a814daab7a62d442eb4b03
JakobLea <jakob.lea@outlook.com>
2023-12-17 17:32:36 +0100

    Lagt til et enkelt bilregister med et felt til alle opplysningene

--- src/main/java/com/example/bilregister/Bil.java
+++ src/main/java/com/example/bilregister/Bil.java
@@ -1,4 +1,19 @@
 package com.example.bilregister;
 
 public class Bil {
+    private String persNum;
+    private String navn;
+    private String adresse;
+    private String bilskilt;
+    private String merke;
+    private String modell;
+
+    public Bil (String persNum, String navn, String adresse, String bilskilt, String merke, String modell) {
+        this.persNum = persNum;
+        this.navn = navn;
+        this.adresse = adresse;
+        this.bilskilt = bilskilt;
+        this.merke = merke;
+        this.modell = modell;
+    }
 }
--- src/main/java/com/example/bilregister/BilController.java
+++ src/main/java/com/example/bilregister/BilController.java
@@ -1,4 +1,16 @@
 package com.example.bilregister;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BilController {
+    private static List<Bil> biler = new ArrayList<>();
+
+    public static void addCar(String persNum, String navn, String adresse, String bilskilt, String merke, String modell) {
+        Bil bil = new Bil(persNum, navn, adresse, bilskilt, merke, modell);
+        biler.add(bil);
+    }
+
+    public static List<Bil> getCars() {
+        return biler;
+    }
 }
